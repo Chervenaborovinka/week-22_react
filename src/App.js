f7a9a6811776a60ec36a24b765e37fbebe92ac22
Liza <yogurtmaniac@gmail.com>
2024-02-18 16:13:29 +0300

    configured images sizes, made flex display on APP.css

--- src/App.js
+++ src/App.js
@@ -6,27 +6,28 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Card
-        title="Sonic X"
-        imgLink='./images/sonic.jpg'
-        description="Ежик супер-герой из одноименной японской видео-игры Sega"
-        superPower='Суперсила: скорость'
-      />
+      <div className='Cards'>
+        <Card
+          title="Sonic X"
+          imgLink="https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8960079_b_v8_ab.jpg"
+          description="Ежик супер-герой из одноименной японской видео-игры Sega"
+          superPower='Суперсила:скорость'
+        />
 
-      <Card
-        title="Super Man"
-        imgLink='./images/227px-Superman01.jpg'
-        description="Вселенский супер герой из комиксов вселенной DC"
-        superPower='Суперсила: Супер мощь'
-      />
+        <Card
+          title="Super Man"
+          imgLink='https://img2.reactor.cc/pics/post/Brandon-Harrelson-Superman-DC-Comics-%D1%84%D1%8D%D0%BD%D0%B4%D0%BE%D0%BC%D1%8B-3439871.jpeg'
+          description="Вселенский супер герой из комиксов вселенной DC"
+          superPower='Суперсила: Супер мощь'
+        />
 
-      <Card
-        title="Catwoman"
-        imgLink='./images/Catwoman.jpg'
-        description="Супер героиня из Готэма"
-        superPower='Суперсила: гибкость и хитрость'
-      />
-
+        <Card
+          title="Catwoman"
+          imgLink="https://www.comic-couch.de/fileadmin/_processed_/d/e/csm_DC_Celebration_-_Catwoman_b29dd5bbf2.jpg"
+          description="Супер героиня из Готэма"
+          superPower='Суперсила: гибкость и хитрость'
+        />
+      </div>
 
     </div>
   );
